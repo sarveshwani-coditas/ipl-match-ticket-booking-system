89545c9a2d754bbd9723f576f37fa64f18904a4a
Sarvesh Wani <sarvesh.wani@coditas.com>
2026-04-22 10:41:18 +0530

    updated

--- src/main/java/com/coditas/iplmatchticketbookingsystem/config/SecurityConfig.java
+++ src/main/java/com/coditas/iplmatchticketbookingsystem/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -24,7 +25,7 @@ public class SecurityConfig {
                 auth
                         .requestMatchers("/api/v1/users/register").permitAll()
                         .requestMatchers("/api/v1/tickets/**").hasRole("USER")
-                        .requestMatchers(HttpMethod.GET,"/api/v1/matches/*").hasRole("USER")
+                        .requestMatchers(HttpMethod.GET,"/api/v1/matches/**").hasAnyRole("USER","ADMIN")
                         .requestMatchers("/api/v1/admin/register",
                                 "/api/v1/standiums",
                                 "/api/v1/matches/**").hasRole("ADMIN")
@@ -32,7 +33,7 @@ public class SecurityConfig {
         );
 
         http.csrf(csrf -> csrf.disable());
-//        http.httpBasic(Customizer.withDefaults());
+        http.httpBasic(Customizer.withDefaults());
 
         return http.build();
     }
